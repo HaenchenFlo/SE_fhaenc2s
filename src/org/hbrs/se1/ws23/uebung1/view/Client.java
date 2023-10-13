d5f098d2026d4e8c081d47f6a572ef3d39aa6261
HaenchenFlo <haenchenflorian@gmail.com>
2023-10-13 16:34:05 +0200

    Client angepasst und Tests implementiert und angepasst

--- src/org/hbrs/se1/ws23/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws23/uebung1/view/Client.java
@@ -1,25 +1,30 @@
 package org.hbrs.se1.ws23.uebung1.view;
 
+import org.hbrs.se1.ws23.uebung1.control.TranslatorFactory;
+import org.hbrs.se1.ws23.uebung1.control.GermanTranslator;
+
 public class Client {
 
-		/*
-		 * Methode zur Ausgabe einer Zahl auf der Console
-		 * (auch bezeichnet als CLI, Terminal)
-		 *
-		 */
-		 void display( int aNumber ){
-			// In dieser Methode soll die Methode translateNumber
-			// mit dem übergegebenen Wert der Variable aNumber
-			// aufgerufen werden.
-			//
-			// Strenge Implementierung gegen das Interface Translator gewuenscht!
+	/*
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 * (auch bezeichnet als CLI, Terminal)
+	 *
+	 */
+	void display( int aNumber ){
+		GermanTranslator translator = TranslatorFactory.createGermanTranslator();
+		String res  = translator.translateNumber(aNumber);
 
-			System.out.println("Das Ergebnis der Berechnung: " +
-					"[das Ergebnis an dieser Stelle]"  );
+		// In dieser Methode soll die Methode translateNumber
+		// mit dem übergegebenen Wert der Variable aNumber
+		// aufgerufen werden.
+		//
+		// Strenge Implementierung gegen das Interface Translator gewuenscht!
 
-		 }
+		System.out.println("Das Ergebnis der Berechnung: " + res);
+
+	}
+
 }
-
 
 
 
--- test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws23/uebung1/test/GermanTranslatorTest.java
@@ -10,7 +10,21 @@ class GermanTranslatorTest {
     @Test
     void aPositiveTest() {
         GermanTranslator translator = new GermanTranslator();
-        String value = translator.translateNumber(0);
-        assertEquals(value, "null");
+
+        String[] zahlen = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"};
+        for (int i = 1; i <= 10; i++) {
+            String value = translator.translateNumber(i);
+            assertEquals(value,zahlen[i-1]);
+            System.out.println("" + i + " ist gleich " + value);
+        }
+    }
+
+    @Test
+    void aNegativeTest() {
+        GermanTranslator translator = new GermanTranslator();
+        assertThrows(IllegalArgumentException.class, () -> translator.translateNumber(15));
+        assertThrows(IllegalArgumentException.class, () -> translator.translateNumber(2));
+        assertThrows(IllegalArgumentException.class, () -> translator.translateNumber(-3));
+        assertThrows(IllegalArgumentException.class, () -> translator.translateNumber(-7));
     }
 }
